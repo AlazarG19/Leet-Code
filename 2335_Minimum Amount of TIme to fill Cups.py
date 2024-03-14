b656c6d4a3236f73b0ac148ec4ac654d748f5de9
AlazarG19 <alazargetachew70@gmail.com>
2024-03-14 17:13:46 +0300

    second question

---
+++ 2335_Minimum Amount of TIme to fill Cups.py
@@ -0,0 +1,18 @@
+def fillCups(self, amount: List[int]) -> int:
+        result = 0
+        while True:
+            amount = [x for x in amount if x >0]
+            if(len(amount) == 0):
+                break
+            elif(len(amount) == 1):
+                result += amount[0]
+                break
+            else:
+                result +=1
+                if len(amount) > 2:
+                    amount.sort(reverse=True)
+                    amount = [x-1 for x in amount[0:2] if x >0] + amount[2:]
+                else:
+                    amount.sort(reverse=True)
+                    amount = [x-1 for x in amount if x >0]
+        return result
