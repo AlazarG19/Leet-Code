c50f0545933af5d2cf2460611e2a760ca8f9f119
AlazarG19 <alazargetachew70@gmail.com>
2024-03-14 17:11:53 +0300

    question 1

---
+++ 2506_Count_Pairs_of_Similar_Strings.py
@@ -0,0 +1,7 @@
+def similarPairs(self, words: List[str]) -> int:
+        output = 0
+        for i in range(len(words)):
+            for j in range(i+1,len(words)):
+                if(set(words[i]) == set(words[j])):
+                    output += 1
+        return output
