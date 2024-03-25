7791a523ed462aa46ff7a7a6a68f6b8036a52801
AlazarG19 <alazargetachew70@gmail.com>
2024-03-25 11:30:10 +0300

    adding comments to each maximum average subarry

--- 643 Maximum Average Subarray.py
+++ 643 Maximum Average Subarray.py
@@ -10,3 +10,8 @@ def findMaxAverage(self, nums: List[int], k: int) -> float:
         end+=1
     return maxavg/k
 
+# solution created using from using sliding window algorithm where
+# instead of creating a subarrray of k elements and calculating the final
+# average add the first k elements and find the highest sum
+# by removing the first element and adding the last element
+# and then finally dividing by k
