df068617dbf7c909c015469729e7f14fde75bd11
AlazarG19 <alazargetachew70@gmail.com>
2024-03-26 11:00:08 +0300

    finalizing the question

--- 438 Find All Anagrams in a string.py
+++ 438 Find All Anagrams in a string.py
@@ -10,7 +10,19 @@ def findAnagrams(self, s: str, p: str) -> List[int]:
         phash[p[i]] = 1 + phash.get(p[i],0)
         shash[s[i]] = 1 + shash.get(s[i],0)
 
-   
+    if shash == phash:
+        answer.append(0)
+    for i in range(len(s)-len(p)):
+        # print(shash,phash,start,end,"-------")
+        shash[s[end]] = 1 + shash.get(s[end],0)
+        if (shash[s[start]] == 1):
+            shash.pop(s[start])
+        else:
+            shash[s[start]] -= 1
+        if (shash == phash):
+            answer.append(i+1)
+        start +=1 
+        end +=1 
     return answer
 
 
