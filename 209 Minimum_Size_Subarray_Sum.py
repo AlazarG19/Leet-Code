7b18162e8d24780cbc4c15612a885348444aa654
AlazarG19 <alazargetachew70@gmail.com>
2024-04-06 09:57:22 +0300

    addin comment

--- 209 Minimum_Size_Subarray_Sum.py
+++ 209 Minimum_Size_Subarray_Sum.py
@@ -1,4 +1,11 @@
 def minSubArrayLen(self, target: int, nums: List[int]) -> int:
+    # use sliding window algorithm and shift r in 
+    # every loop to the right while doing 
+    # this add up the element at posistion [right]
+    # to a variable total 
+    #  once that is done whenver
+    # total greater than target shift left to the 
+    # right to find minimum length
     left,total = 0,0
     res = len(nums) +1
     for r in range(len(nums)):
